4647bf84976f9f8b0d3ad907cad49d3fadeb72b9
luancaius <luancaius@outlook.com>
2020-06-06 01:59:20 -0400

    adding url env

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5db8f1ee2167469187cc7bd5615a83d-1379214937.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
